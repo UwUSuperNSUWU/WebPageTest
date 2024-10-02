25bd9ade41d245e9776439b9c6e58ff315fd7643
supernsprivado@gmail.com <supernsprivado@gmail.com>
2024-10-02 10:10:04 -0300

    Test

---
+++ script/script.js
@@ -0,0 +1,6 @@
+window.addEventListener("load", function () {
+  setTimeout(function () {
+    var loader = document.getElementById("carga");
+    loader.style.display = "none";
+  }, 3000); // 3000 milisegundos = 3 segundos
+});
